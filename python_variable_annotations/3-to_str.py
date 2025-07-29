e5f8014bc0eed2e8ac0fdafa0102dac3e62db60a
JosephGleason <9719@holbertonstudents.com>
2025-07-29 13:29:20 -0400

    added task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that converts a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+    """
+    return str(n)
