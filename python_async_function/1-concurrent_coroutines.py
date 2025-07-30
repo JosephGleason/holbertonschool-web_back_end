f622cf9f921b601e6ff07eb44b6705457a933595
JosephGleason <9719@holbertonstudents.com>
2025-07-30 16:12:52 -0400

    pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,13 +8,14 @@ from importlib import import_module
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """Runs wait_random n times concurrently with the given max_delay.
-    
+
     Args:
         n (int): Number of coroutines to launch.
         max_delay (int): Max delay for each coroutine.
-    
+
     Returns:
         List[float]: List of delays in ascending order.
     """
