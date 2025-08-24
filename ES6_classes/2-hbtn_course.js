f5d8a2b1969da62cebed0784199fefc5dc31bfbf
Joseph Gleason <joeg1834@gmail.com>
2025-08-24 15:15:16 -0400

    added getter and setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
     constructor (name, length, students) {
         if (typeof name !== 'string') {
             throw new TypeError('Name must be a string');
@@ -13,5 +13,37 @@ class HolbertonCourse {
         this._name = name;
         this._length = length;
         this._students = students;
+    }
+
+    get name () {
+        return this._name;
+    }
+    
+    set name (person) {
+        if (typeof person !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = person;
+    }
+    
+    get length () {
+        return this._length;
+    }
+    set length (long) {
+        if (typeof long !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = long;
+    }
+
+    get students () {
+        return this._students;
+    }
+
+    set students (newStudents) {
+        if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = newStudents;
     }
 }
