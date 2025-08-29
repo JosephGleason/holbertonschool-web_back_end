31b0fcac5028f644ade7723afa58e9851eec1095
Joseph Gleason <joeg1834@gmail.com>
2025-08-29 16:22:42 -0400

    added task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+    if (startString == "") {
+        return "";
+    }
+    const result = [];
+    for (const value of set) {
+        if (value.startsWith(startString)) {
+            const sliced = value.slice(startString.length);
+            result.push(sliced);
+        }
+    }
+    return result.join("-");
+}
