3923410a6c7a776e87aed9812ab6fa7c1f7ae1c4
josephg <9719@holbertonstudents.com>
2025-09-06 14:04:23 -0400

    fixed everything lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
-    process.stdout.write(`Your name is: ${input}`)
-})
+  process.stdout.write(`Your name is: ${input}`);
+});
 
-process.stdin.on("end", () => {
-    console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
