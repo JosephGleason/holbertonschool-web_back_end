c66925142cfad39e40334e3ed17a9ee26916cb06
josephg <9719@holbertonstudents.com>
2025-09-06 14:00:54 -0400

    added single quotes.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
-process.stdin.on("data", (input) => {
+process.stdin.on('data', (input) => {
     process.stdout.write(`Your name is: ${input}`)
 })
 
 process.stdin.on("end", () => {
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
 });
