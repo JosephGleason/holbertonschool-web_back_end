d36a5283e45155b39b63fec1f3920a5f97226390
josephg <9719@holbertonstudents.com>
2025-09-06 13:57:08 -0400

    fixed typo

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.setEncoding('utf8');
-process.stdin.on("data", (input)=> {
+process.stdin.on("data", (input) => {
     process.stdout.write(`Your name is: ${input}`)
 })
 
